c39c76dacfad8631425d5aceb21788bd580fe831
hayatoo20 <118213861+hayatoo20@users.noreply.github.com>
2024-06-15 19:18:03 +0400

    Add files via upload

---
+++ ProductObj.cs
@@ -0,0 +1,277 @@
+﻿using EShopping.Admin;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+
+namespace EShopping
+{
+    public class ProductObj
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public string AdditionalDescription { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Size { get; set; }
+        public string Color { get; set; }
+        public string CompanyName { get; set; }
+        public string Tags { get; set; }
+        public int CategoryId { get; set; }
+        public int SubCategoryId { get; set; }
+        public bool IsCustomized { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public List<ProductImageObj> ProductImages { get; set; } = new List<ProductImageObj>();
+        public int DefaultImagePosition { get; set; }
+    }
+
+    public class ProductImageObj
+    {
+        public int ImageId { get; set; }
+        public string ImageUrl { get; set; }
+        public int ProductId { get; set; }
+        public bool DefaultImage { get; set; }
+    }
+
+    public class ProductDAL
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        SqlDataAdapter sda;
+        DataTable dt;
+        SqlTransaction transaction = null;
+        public int AddUpdateProduct(ProductObj productBO)
+        {
+            int result = 0;
+            int productId = 0;
+            string type = "insert";
+            using (con = new SqlConnection(Utils.getConnection()))
+            {
+                try
+                {
+                    var productImages = productBO.ProductImages;
+                    #region Insert Product
+
+                    con.Open();
+                    transaction = con.BeginTransaction();
+                    productId = productBO.ProductId;
+                    cmd = new SqlCommand("Product_Crud", con, transaction);
+                    cmd.Parameters.AddWithValue("@Action", productId == 0 ? "INSERT" : "UPDATE");
+                    cmd.Parameters.AddWithValue("@ProductName", productBO.ProductName);
+                    cmd.Parameters.AddWithValue("@ShortDescription", productBO.ShortDescription);
+                    cmd.Parameters.AddWithValue("@LongDescription", productBO.LongDescription);
+                    cmd.Parameters.AddWithValue("@AdditionalDescription", productBO.AdditionalDescription);
+                    cmd.Parameters.AddWithValue("@Price", productBO.Price);
+                    cmd.Parameters.AddWithValue("@Quantity", productBO.Quantity);
+                    cmd.Parameters.AddWithValue("@Size", productBO.Size);
+                    cmd.Parameters.AddWithValue("@Color", productBO.Color);
+                    cmd.Parameters.AddWithValue("@CompanyName", productBO.CompanyName);
+                    cmd.Parameters.AddWithValue("@CategoryId", productBO.CategoryId);
+                    cmd.Parameters.AddWithValue("@SubCategoryId", productBO.SubCategoryId);
+                    cmd.Parameters.AddWithValue("@Tags", productBO.Tags);
+                    cmd.Parameters.AddWithValue("@IsCustomized", productBO.IsCustomized);
+                    cmd.Parameters.AddWithValue("@IsActive", productBO.IsActive);
+                    if (productId > 0)
+                    {
+                        cmd.Parameters.AddWithValue("@ProductId", productBO.ProductId);
+                        type = "update";
+                    }
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+
+                    if (productId == 0)
+                    {
+                        cmd = new SqlCommand("Product_Crud", con, transaction);
+                        cmd.Parameters.AddWithValue("@Action", "RECENT_PRODUCT");
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        dr = cmd.ExecuteReader();
+                        while (dr.Read())
+                        {
+                            productId = (int)dr["ProductId"];
+                        }
+                        dr.Close();
+                    }
+                    #endregion
+
+                    #region Insert Product Images
+                    if (productId > 0)
+                    {
+                        if (type == "insert") //insert images
+                        {
+                            foreach (var image in productImages)
+                            {
+                                cmd = new SqlCommand("Product_Crud", con, transaction);
+                                cmd.Parameters.AddWithValue("@Action", "INSERT_PROD_IMG");
+                                cmd.Parameters.AddWithValue("@ImageUrl", image.ImageUrl);
+                                cmd.Parameters.AddWithValue("@ProductId", productId);
+                                cmd.Parameters.AddWithValue("@DefaultImage", image.DefaultImage);
+                                cmd.CommandType = CommandType.StoredProcedure;
+                                cmd.ExecuteNonQuery();
+                                result = 1;
+                            }
+                        }
+                        else //update images
+                        {
+                            bool isTrue = false;
+                            if (productImages.Count != 0)
+                            {
+                                cmd = new SqlCommand("Product_Crud", con, transaction);
+                                cmd.Parameters.AddWithValue("@Action", "DELETE_PROD_IMG");
+                                cmd.Parameters.AddWithValue("@ProductId", productId);
+                                cmd.CommandType = CommandType.StoredProcedure;
+                                cmd.ExecuteNonQuery();
+                                isTrue = true;
+                            }
+                            else
+                            {
+                                int defaultImagePos = productBO.DefaultImagePosition;
+                                if (defaultImagePos > 0)
+                                {
+                                    cmd = new SqlCommand("Product_Crud", con, transaction);
+                                    cmd.Parameters.AddWithValue("@Action", "UPDATE_IMG_POS");
+                                    cmd.Parameters.AddWithValue("@ProductId", productId);
+                                    cmd.Parameters.AddWithValue("@DefaultImagePos", defaultImagePos);
+                                    cmd.CommandType = CommandType.StoredProcedure;
+                                    cmd.ExecuteNonQuery();
+                                    result = 1;
+                                }
+                                else
+                                {
+                                    result = 1;
+                                }
+                            }
+                            if (isTrue)
+                            {
+                                foreach (var image in productImages)
+                                {
+                                    cmd = new SqlCommand("Product_Crud", con, transaction);
+                                    cmd.Parameters.AddWithValue("@Action", "INSERT_PROD_IMG");
+                                    cmd.Parameters.AddWithValue("@ImageUrl", image.ImageUrl);
+                                    cmd.Parameters.AddWithValue("@DefaultImage", image.DefaultImage);
+                                    cmd.Parameters.AddWithValue("@ProductId", productId);
+                                    cmd.CommandType = CommandType.StoredProcedure;
+                                    cmd.ExecuteNonQuery();
+                                    result = 1;
+                                }
+                            }
+                        }
+
+                    }
+                    #endregion
+
+                    transaction.Commit();
+                }
+                catch (Exception)
+                {
+                    try
+                    {
+                        transaction.Rollback();
+                        result = 0;
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+            return result;
+        }
+
+        public DataTable ProductsWithDefaultImg()
+        {
+            try
+            {
+                using (con = new SqlConnection(Utils.getConnection()))
+                {
+                    con.Open();
+                    cmd = new SqlCommand("Product_Crud", con);
+                    cmd.Parameters.AddWithValue("@Action", "SELECT");
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    sda = new SqlDataAdapter(cmd);
+                    dt = new DataTable();
+                    sda.Fill(dt);
+                    return dt;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public DataTable ProductByIdWithImages(int productId)
+        {
+            try
+            {
+                DataTable dt = ProductById(productId);
+                dt.Columns.Add("Image2");
+                dt.Columns.Add("Image3");
+                dt.Columns.Add("Image4");
+                dt.Columns.Add("DefaultImage");
+                DataRow dr = dt.NewRow();
+                string images = dt.Rows[0]["Image1"].ToString();
+                string[] imgArr = images.Split(';');
+                string imag;
+                int rb = 0;
+                foreach (string img in imgArr)
+                {
+                    imag = img.Substring(img.IndexOf(": ") + 1);
+                    if (imag.Trim() == "1")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        rb++;
+                    }
+                }
+
+                foreach (DataRow dataRow in dt.Rows)
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        dataRow["image" + (i + 1)] = imgArr[i].Trim();
+                    }
+                    dataRow["DefaultImage"] = rb;
+                }
+                return dt;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public DataTable ProductById(int pId)
+        {
+            try
+            {
+                using (con = new SqlConnection(Utils.getConnection()))
+                {
+                    con.Open();
+                    cmd = new SqlCommand("Product_Crud", con);
+                    cmd.Parameters.AddWithValue("@Action", "GETBYID");
+                    cmd.Parameters.AddWithValue("@ProductId", pId);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    sda = new SqlDataAdapter(cmd);
+                    dt = new DataTable();
+                    sda.Fill(dt);
+                    return dt;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,322 @@
+﻿using EShopping.User;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace EShopping
+{
+    public class Utils
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+        SqlDataReader sdr;
+        DataTable dt;
+        public static string getConnection()
+        {
+            return ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+        }
+
+        public static bool IsValidExtension(string fileName)
+        {
+            bool isValid = false;
+            string[] fileExtension = { ".jpg", ".png", ".jpeg" };
+            for (int i = 0; i <= fileExtension.Length - 1; i++)
+            {
+                if (fileName.Contains(fileExtension[i]))
+                {
+                    isValid = true;
+                    break;
+                }
+            }
+            return isValid;
+        }
+
+        // Generates Unique ID/VALUE
+        public static string GetUniqueId()
+        {
+            Guid obj = Guid.NewGuid();
+            return obj.ToString();
+        }
+
+        // Setting default image if their is no image for any job.
+        public static string GetImageUrl(Object url)
+        {
+            string url1 = "";
+            if (string.IsNullOrEmpty(url.ToString()) || url == DBNull.Value)
+            {
+                url1 = "../Images/No_image.png";
+            }
+            else
+            {
+                url1 = string.Format("../{0}", url);
+            }
+            return url1;
+        }
+
+        public static string[] getImagesPath(string[] imagesArr)
+        {
+            List<string> list = new List<string>();
+            string[] imagesPath = null;
+            string fileExtension = string.Empty;
+            for (int i = 0; i <= imagesArr.Length - 1; i++)
+            {
+                fileExtension = Path.GetExtension(imagesArr[i]);
+                list.Add("Images/Product/" + GetUniqueId().ToString() + fileExtension);
+            }
+            return imagesPath = list.ToArray();
+        }
+
+        public static string getItemWithCommaSeparater(ListBox listBox)
+        {
+            string selectedItem = string.Empty;
+            foreach (int i in listBox.GetSelectedIndices())
+            {
+                selectedItem += listBox.Items[i].Text + ",";
+            }
+            return selectedItem;
+        }
+
+        public int WishlistItemExist(int productId)
+        {
+            using (con = new SqlConnection(getConnection()))
+            {
+                try
+                {
+                    cmd = new SqlCommand("Wishlist_Crud", con);
+                    cmd.Parameters.AddWithValue("@Action", "ITEMEXIST");
+                    cmd.Parameters.AddWithValue("@ProductId", productId);
+                    cmd.Parameters.AddWithValue("@UserId", System.Web.HttpContext.Current.Session["userId"]);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    sda = new SqlDataAdapter(cmd);
+                    dt = new DataTable();
+                    sda.Fill(dt);
+                    cmd.Dispose();
+                    return dt.Rows.Count;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        public int AddToWishlist(int productId)
+        {
+            using (con = new SqlConnection(getConnection()))
+            {
+                try
+                {
+                    int result = 0;
+                    cmd = new SqlCommand("Wishlist_Crud", con);
+                    cmd.Parameters.AddWithValue("@Action", "INSERT");
+                    cmd.Parameters.AddWithValue("@ProductId", productId);
+                    cmd.Parameters.AddWithValue("@UserId", System.Web.HttpContext.Current.Session["userId"]);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    result = 1;
+                    cmd.Dispose();
+                    return result;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        public int itemCount(string type)
+        {
+            using (con = new SqlConnection(getConnection()))
+            {
+                try
+                {
+                    int result = 0;
+                    string spName = type == "cart" ? "Cart_Crud" : "Wishlist_Crud";
+                    cmd = new SqlCommand(spName, con);
+                    cmd.Parameters.AddWithValue("@Action", "GETITEMCOUNT");
+                    cmd.Parameters.AddWithValue("@UserId", System.Web.HttpContext.Current.Session["userId"]);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    sdr = cmd.ExecuteReader();
+                    while (sdr.Read())
+                    {
+                        result = sdr.GetInt32(0);
+                    }
+                    cmd.Dispose();
+                    return result;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        public int cartItemExistReturnQuantity(int productId)
+        {
+            using (con = new SqlConnection(getConnection()))
+            {
+                try
+                {
+                    cmd = new SqlCommand("Cart_Crud", con);
+                    cmd.Parameters.AddWithValue("@Action", "GETBYID");
+                    cmd.Parameters.AddWithValue("@ProductId", productId);
+                    cmd.Parameters.AddWithValue("@UserId", System.Web.HttpContext.Current.Session["userId"]);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    sda = new SqlDataAdapter(cmd);
+                    dt = new DataTable();
+                    sda.Fill(dt);
+                    cmd.Dispose();
+                    int result = dt.Rows.Count;
+                    int quantity = 0;
+                    if (result == 1)
+                    {
+                        quantity = Convert.ToInt32( dt.Rows[0]["Quantity"]);
+                    }
+                    return quantity;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        public int addOrUpdateCartItem(int productId, int quantity, string actionType)
+        {
+            using (con = new SqlConnection(getConnection()))
+            {
+                try
+                {
+                    int result = 0;
+                    //cmd = new SqlCommand("Insert into Cart values(@ProductId,@Quantity,@UserId,@CreatedDate)", con);
+                    cmd = new SqlCommand("Cart_Crud", con);
+                    cmd.Parameters.AddWithValue("@Action", actionType);
+                    cmd.Parameters.AddWithValue("@ProductId", productId);
+                    cmd.Parameters.AddWithValue("@Quantity", quantity);
+                    cmd.Parameters.AddWithValue("@UserId", System.Web.HttpContext.Current.Session["userId"]);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    result = 1;
+                    cmd.Dispose();
+                    return result;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        public DataTable CartItemByUserId()
+        {
+            try
+            {
+                using (con = new SqlConnection(getConnection()))
+                {
+                    cmd = new SqlCommand("Cart_Crud", con);
+                    cmd.Parameters.AddWithValue("@Action", "SELECTBYUSERID");
+                    cmd.Parameters.AddWithValue("@UserId", System.Web.HttpContext.Current.Session["userId"]);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    sda = new SqlDataAdapter(cmd);
+                    dt = new DataTable();
+                    sda.Fill(dt);
+                    return dt;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public int deleteCartItem(int productId)
+        {
+            using (con = new SqlConnection(getConnection()))
+            {
+                try
+                {
+                    int result = 0;
+                    cmd = new SqlCommand("Cart_Crud", con);
+                    cmd.Parameters.AddWithValue("@Action", "DELETE");
+                    cmd.Parameters.AddWithValue("@ProductId", productId);
+                    cmd.Parameters.AddWithValue("@UserId", System.Web.HttpContext.Current.Session["userId"]);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    result = 1;
+                    cmd.Dispose();
+                    return result;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        public bool updateCartQuantity(int quantity, int productId)
+        {
+            bool isUpdated = false;
+            con = new SqlConnection(getConnection());
+            cmd = new SqlCommand("Cart_Crud", con);
+            cmd.Parameters.AddWithValue("@Action", "UPDATE");
+            cmd.Parameters.AddWithValue("@ProductId", productId);
+            cmd.Parameters.AddWithValue("@Quantity", quantity);
+            cmd.Parameters.AddWithValue("@UserId", System.Web.HttpContext.Current.Session["userId"]);
+            cmd.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                isUpdated = true;
+            }
+            catch (Exception ex)
+            {
+                isUpdated = false;
+                System.Web.HttpContext.Current.Response.Write("<script>alert('Error - " + ex.Message + "');</script>");
+            }
+            finally
+            {
+                con.Close();
+            }
+            return isUpdated;
+        }
+
+        public int dashboardCount(string name)
+        {
+            int count = 0;
+            con = new SqlConnection(getConnection());
+            cmd = new SqlCommand("Dashboard", con);
+            cmd.Parameters.AddWithValue("@Action", name);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            sdr = cmd.ExecuteReader();
+            while (sdr.Read())
+            {
+                if (sdr[0] == DBNull.Value)
+                {
+                    count = 0;
+                }
+                else
+                {
+                    count = Convert.ToInt32(sdr[0]);
+                }
+            }
+            sdr.Close();
+            con.Close();
+            return count;
+        }
+
+    }
+}
